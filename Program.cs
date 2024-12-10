3c8b8ee0b28b3a09b2a43f0f45a303f0bfb2e70d
Alekslashko <alexandarshou21a@gmail.com>
2024-12-10 15:18:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    public static void Main(string[]arg)
+    {
+        decimal balance = 100;
+
+        Thread clientThread = new Thread(() =>
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                decimal amountToWithdraw = 300;
+                Console.WriteLine($"Клиент иска да изтегли {amountToWithdraw} лв.");
+                if (amountToWithdraw <= balance)
+                {
+                    balance -= amountToWithdraw;
+                    Console.WriteLine($"Клиент успешно изтегли {amountToWithdraw} лв.");
+                }
+                else {Console.WriteLine($"Клиент не може да изтегли {amountToWithdraw} лв. тъй като има {balance} лв.");}
+            }
+        });
+
+        Thread bankEmployeeThread = new Thread(() =>
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                decimal amountToDeposit = 200;
+                balance += amountToDeposit;
+                Console.WriteLine($"Банков служител добави {amountToDeposit}");
+            }
+        });
+
+    clientThread.Start();
+
+
+    }
+}
